c917ada45d0d07a6faea9a63dc666641e912eda6
wachirawahomed <wachirawahomed@gmail.com>
2024-01-24 05:57:54 +0300

    ppython classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class
+
+This is a class that defines a square.
+
+"""
+
+
+class Square():
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square module.
+
+This is a class that defines a square and it's init method
+
+"""
+
+class Square():
+
+    def __init__(self, size=0):
+        """Sets the necessary attributes for the Square object.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
