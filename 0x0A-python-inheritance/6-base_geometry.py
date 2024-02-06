b4b7b6d0b7ed5d94809d74cbec7c80561cc4a473
wachirawahomed <wachirawahomed@gmail.com>
2024-02-06 04:58:15 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""base_geometry module
+
+Contains class BassGeometry and method area
+"""
+
+
+class BaseGeometry:
+    """Empty class representing the base geometry."""
+
+    def area(self):
+        """Raise an exception with a message"""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""base_geometry module
+
+Contains class BaseGeometry and some methods
+"""
+
+
+class BaseGeometry:
+    """Empty class representing the base geometry."""
+
+    def area(self):
+        """Raise an exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate the given value.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
