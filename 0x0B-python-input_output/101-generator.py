d28f23abc46c53e1bc35fd8a15f40814be4d325f
wachirawahomed <wachirawahomed@gmail.com>
2024-02-08 02:52:23 +0300

    101-generator.py

--- 0x0B-python-input_output/101-generator.py
+++ 0x0B-python-input_output/101-generator.py
@@ -5,45 +5,37 @@ import sys
 total_size = 0
 codes = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
+iteration = 0
 
 
 def print_stats():
-    """Function that prints a summary of the statistics."""
-    print("Total file size: {}".format(total_size))
-    for code, count in sorted(codes.items()):
-        if count > 0:
-            print("Number of lines with status code\
-                    {}: {}".format(code, count))
+    """Function that prints a resume of the stats."""
+    print("File size: {}".format(total_size))
+    for k, v in sorted(codes.items()):
+        if v is not 0:
+            print("{}: {}".format(k, v))
 
 
 try:
-    for line_number, line in enumerate(sys.stdin, start=1):
-        parts = line.split()
-        if len(parts) >= 3:
-            status_code = parts[-2]
+    for line in sys.stdin:
+        line = line.split()
+        if len(line) >= 2:
+            tmp = iteration
+            if line[-2] in codes:
+                codes[line[-2]] += 1
+                iteration += 1
             try:
-                file_size = int(parts[-1])
-            except ValueError:
-                print("Error: Line {}: Invalid file size\
-                        format: {}".format(line_number, parts[-1]))
-                continue
+                total_size += int(line[-1])
+                if tmp == iteration:
+                    iteration += 1
+            except:
+                if tmp == iteration:
+                    continue
 
-            if status_code in codes:
-                codes[status_code] += 1
-            else:
-                print("Error: Line {}: Unknown status\
-                        code: {}".format(line_number, status_code))
-                continue
-
-            total_size += file_size
-
-        if line_number % 10 == 0:
-            print("\nStatistics at line {}: ".format(line_number))
+        if iteration % 10 == 0:
             print_stats()
 
-    print("\nFinal statistics: ")
     print_stats()
 
 except KeyboardInterrupt:
-    print("\nFinal statistics (interrupted): ")
     print_stats()
