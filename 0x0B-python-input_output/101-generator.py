095b845fa64d09a31bebfebe7bfe7e10448c6faf
wachirawahomed <wachirawahomed@gmail.com>
2024-02-08 03:00:21 +0300

    101-generator.py

--- 0x0B-python-input_output/101-generator.py
+++ 0x0B-python-input_output/101-generator.py
@@ -1,52 +1,27 @@
 #!/usr/bin/python3
+"""Log parsing script."""
 import sys
+import random
+import datetime
+from time import sleep
 
 
-def print_info():
-    print('File size: {:d}'.format(file_size))
+def generate_log_line():
+    """Generate a random log line."""
+    return "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        random.randint(1, 255), datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024))
 
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
+
+def main():
+    """Main function to generate log lines."""
+    for i in range(10000):
+        sleep(random.random())
+        sys.stdout.write(generate_log_line())
+        sys.stdout.flush()
 
 
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
-lc = 0
-file_size = 0
-
-try:
-    for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
-
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
-
-        lc += 1
-
-    print_info()
-except KeyboardInterrupt:
-    print_info()
-    raise
+if __name__ == "__main__":
+    main()
