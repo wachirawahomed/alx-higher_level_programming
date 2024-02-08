bdbfb132dbffaae4b0d81c6672c68fa79d0827ad
wachirawahomed <wachirawahomed@gmail.com>
2024-02-08 03:17:03 +0300

    101

--- 0x0B-python-input_output/101-generator.py
+++ 0x0B-python-input_output/101-generator.py
@@ -2,25 +2,30 @@
 """Log parsing script."""
 import sys
 import random
-import datetime
+from datetime import datetime
 from time import sleep
 
 
 def generate_log_line():
     """Generate a random log line."""
-    return "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        random.randint(1, 255), datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024))
+    return "{:d}.{:d}.{:d}.{:d} - [{}] \"GET/projects\
+            /260 HTTP/1.1\" {} {}\n".format(
+                random.randint(1, 255), random.randint(1, 255),
+                random.randint(1, 255), random.randint(1, 255),
+                datetime.now(),
+                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+                random.randint(1, 1024))
 
 
 def main():
-    """Main function to generate log lines."""
-    for i in range(10000):
-        sleep(random.random())
-        sys.stdout.write(generate_log_line())
-        sys.stdout.flush()
+    """Main function."""
+    try:
+        while True:
+            print(generate_log_line(), end="")
+            sys.stdout.flush()
+            sleep(random.random())
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
