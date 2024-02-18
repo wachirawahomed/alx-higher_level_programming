13f28abe261e56fad55eee6f578c4cb0ab0ae7a8
wachirawahomed <wachirawahomed@gmail.com>
2024-02-18 19:04:13 +0300

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Base module.
+
+Contains the Base class which will be the
+“base” of all other classes in this project.
+"""
+
+
+class Base:
+    """Base class for managing id attribute."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes Base instance
+
+        Args:
+            id (init): the identifier of the Base object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
