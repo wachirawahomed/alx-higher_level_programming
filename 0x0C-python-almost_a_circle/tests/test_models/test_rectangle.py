097b5af6eeff24744b0dd469f87f3cb3a19727d9
wachirawahomed <wachirawahomed@gmail.com>
2024-03-03 01:47:59 +0300

    task 4

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,6 +3,7 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 
+
 class TestRectangle(unittest.TestCase):
 
     def test_constructor_with_arguments(self):
@@ -86,6 +87,16 @@ class TestRectangle(unittest.TestCase):
 
     def test_inheritance_from_base(self):
         self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_area_method(self):
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
 
     def test_pep8_compliance(self):
         # Ensure that the Rectangle class implementation follows PEP 8 guidelines
