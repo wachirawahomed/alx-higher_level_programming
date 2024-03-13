bfdce48913d33d984fd38322830eb279e29ac7b6
wachirawahomed <wachirawahomed@gmail.com>
2024-03-13 16:39:02 -0700

    updates

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,17 +1,20 @@
 -- Uses the hbtn_0d_tvshows database to list all genres
 -- not linked to the show Dexter
 
--- Get genres linked to Dexter
-SELECT genre_id
-FROM tv_show_genres
-WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter');
+SELECT DISTINCT tv_genres.name
+  FROM tv_shows
+       JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
 
--- Get genres not linked to Dexter
-SELECT tv_genres.name
-FROM tv_genres
-WHERE id NOT IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    WHERE tv_show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+       JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_shows
+         JOIN tv_show_genres
+         ON tv_shows.id = tv_show_genres.show_id
+
+         JOIN tv_genres
+         ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
 )
-ORDER BY tv_genres.name ASC;
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,19 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 
--- Get shows linked to Comedy
-SELECT show_id
-FROM tv_show_genres
-WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy');
+SELECT DISTINCT tv_shows.title
+  FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
 
--- Get shows without Comedy genre
-SELECT tv_shows.title
-FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title NOT IN (
+    SELECT tv_shows.title
+      FROM tv_shows
+           JOIN tv_show_genres
+           ON tv_shows.id = tv_show_genres.show_id
+
+           JOIN tv_genres
+           ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
 )
-ORDER BY tv_shows.title ASC;
