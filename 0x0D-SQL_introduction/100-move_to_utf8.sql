90c94334afb4fed0f35b8107f794e1495785f2ab
wachirawahomed <wachirawahomed@gmail.com>
2024-03-13 19:05:03 +0300

    Advance task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- Task: Convert to UTF8
 -- Lonverts the hbtn_0c_0 database, the first_table table, and the name field in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci).
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`;
