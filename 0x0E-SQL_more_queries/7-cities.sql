39f05b95029d8a8c62743b04b651c78b640b7504
wachirawahomed <wachirawahomed@gmail.com>
2024-03-13 13:39:14 -0700

    mandatory task

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- creates the database hbtn_0d_usa and the table cities
+-- (in the database hbtn_0d_usa) on MySQL server.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Lists all cities contained in the database hbtn_0d_usa.
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
