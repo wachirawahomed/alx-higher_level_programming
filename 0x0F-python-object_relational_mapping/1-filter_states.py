7baab84ae709f6d1458230c3536738c0e6344ed7
wachirawahomed <wachirawahomed@gmail.com>
2024-04-19 05:54:17 +0300

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """Script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+from the database hbtn_0e_0_usa
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
 
 import sys
 import MySQLdb
