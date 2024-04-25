9943b8fc67ae26fdef83afaba9dfcd87795dce6e
wachirawahomed <wachirawahomed@gmail.com>
2024-04-25 14:49:23 -0700

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file as the request body and displays the body of the response
-curl -sX POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
