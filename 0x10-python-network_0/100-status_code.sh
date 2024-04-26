a0d0c84e1096b88d20599aaeea46c65559d7311b
wachirawahomed <wachirawahomed@gmail.com>
2024-04-26 20:54:03 +0300

    task 7 & 9

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sending a GET request to the URL and extracting the status code from the response header
-curl -sI "$1" | grep -oP "(?<=HTTP\/1\.1 )\d+"
+curl -sI -o /dev/null -w "%{http_code}" "$1"
