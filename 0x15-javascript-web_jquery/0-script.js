6e775168d5796330f7833dc129355bcbfc7de2d9
wachirawahomed <wachirawahomed@gmail.com>
2025-02-16 00:12:11 +0300

    0-script.js v.2

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,5 @@
-ddEventListener('DOMContentLoaded', function() {
-	  document.querySelector('header').style.color = '#FF0000';
-});
+// Selecting the <header> element using querySelector
+const header = document.querySelector('header');
+
+// Changing the text color to red (#FF0000)
+header.style.color = '#FF0000';
